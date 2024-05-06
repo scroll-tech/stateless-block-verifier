cab7864a8ba928f30d6368953fcb7335987e99d0
lightsing <light.tsing@gmail.com>
2024-05-06 19:57:06 +0800

    add timing
    (cherry picked from commit b16dc1af44a3cc7b337a37bce69e9ed2a22c83fe)

--- src/bin/trace-verifier/utils.rs
+++ src/bin/trace-verifier/utils.rs
@@ -6,13 +6,15 @@ pub fn verify(l2_trace: BlockTrace) {
     let root_after = l2_trace.storage_trace.root_after.to_word();
     info!("Root after in trace: {:x}", root_after);
 
+    let now = std::time::Instant::now();
     let mut executor = EvmExecutor::new(&l2_trace);
     let revm_root_after = executor.handle_block(&l2_trace).to_word();
     info!("Root after in revm: {:x}", revm_root_after);
+    let elapsed = now.elapsed();
 
     if root_after != revm_root_after {
         error!("Root mismatch");
         std::process::exit(1);
     }
-    info!("Root matches");
+    info!("Root matches in: {} ms", elapsed.as_millis());
 }
