0447c79b52911459cc800b117a4044f0ddcbcec1
lightsing <light.tsing@gmail.com>
2024-07-09 13:54:37 +0800

    bps counter

--- src/bin/trace-verifier/utils.rs
+++ src/bin/trace-verifier/utils.rs
@@ -1,6 +1,7 @@
 use eth_types::l2_types::BlockTrace;
 use eth_types::ToWord;
 use stateless_block_verifier::{EvmExecutor, HardforkConfig};
+use std::time::Instant;
 
 pub fn verify(
     l2_trace: BlockTrace,
@@ -8,11 +9,14 @@ pub fn verify(
     disable_checks: bool,
     log_error: bool,
 ) -> bool {
+    static BLOCK_COUNTER: std::sync::atomic::AtomicUsize = std::sync::atomic::AtomicUsize::new(0);
+    static LAST_TIME: std::sync::Mutex<Instant> = std::sync::Mutex::new(Instant::now());
+
     trace!("{:#?}", l2_trace);
     let root_after = l2_trace.storage_trace.root_after.to_word();
     info!("Root after in trace: {:x}", root_after);
 
-    let now = std::time::Instant::now();
+    let now = Instant::now();
 
     #[cfg(feature = "profiling")]
     let guard = pprof::ProfilerGuardBuilder::default()
@@ -50,5 +54,16 @@ pub fn verify(
         return false;
     }
     info!("Root matches in: {} ms", elapsed.as_millis());
+
+    let block_counter = BLOCK_COUNTER.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+    if block_counter > 50 {
+        let mut last_time = LAST_TIME.lock().unwrap();
+        let blocks = BLOCK_COUNTER.swap(0, std::sync::atomic::Ordering::SeqCst);
+        let elapsed = last_time.elapsed().as_secs_f64();
+        let bps = blocks as f64 / elapsed;
+        info!("Blocks per second: {:.2}", bps);
+        *last_time = Instant::now();
+    }
+
     true
 }
