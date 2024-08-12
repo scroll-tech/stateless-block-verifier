d9371c17a51a8d95917bd343467779b0679f2b70
lightsing <light.tsing@gmail.com>
2024-08-12 15:08:59 +0800

    submodulize testdata

--- scripts/download_trace.sh
+++
@@ -1,3 +0,0 @@
-set -x
-blk=5224657
-curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"scroll_getBlockTraceByNumberOrHash", "params": ["'$(printf '0x%x' $blk)'"], "id": 99}' 127.0.0.1:8545 > testdata/mainnet_blocks/${blk}.json 
