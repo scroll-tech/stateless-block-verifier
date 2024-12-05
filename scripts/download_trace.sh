ab3fa4a2d700089bc2c08388952b728b46324d5a
lightsing <light.tsing@gmail.com>
2024-12-05 13:39:49 +0800

    update download script

--- scripts/download_trace.sh
+++ scripts/download_trace.sh
@@ -1,3 +1,20 @@
-set -x
-blk=5224657
-curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"scroll_getBlockTraceByNumberOrHash", "params": ["'$(printf '0x%x' $blk)'"], "id": 99}' 127.0.0.1:8545 > testdata/mainnet_blocks/${blk}.json 
+set -ex
+
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 <block_number> [url] [output_dir]"
+    exit 0
+fi
+
+BLOCK=$1
+URL=${2:-http://localhost:8545}
+OUT_DIR=${3:-testdata/holesky_witness}
+
+HEX_BLOCK=$(printf '0x%x' $BLOCK)
+
+mkdir -p ${OUT_DIR}/${HEX_BLOCK}
+
+PAYLOAD='{"jsonrpc":"2.0","method":"debug_executionWitness", "params": ["'$HEX_BLOCK'"], "id": 1}'
+curl -H "Content-Type: application/json" -X POST --data "$PAYLOAD" $URL | jq .result > ${OUT_DIR}/${HEX_BLOCK}/witness.json
+
+PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber", "params": ["'$HEX_BLOCK'", true], "id": 1}'
+curl -H "Content-Type: application/json" -X POST --data "$PAYLOAD" $URL | jq .result > ${OUT_DIR}/${HEX_BLOCK}/block.json
