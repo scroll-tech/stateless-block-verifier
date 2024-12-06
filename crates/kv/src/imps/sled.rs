7c316505684902dcf37b12d1705abd9acb6ca4f3
lightsing <light.tsing@gmail.com>
2024-12-06 13:05:34 +0800

    add kv and trie crate

---
+++ crates/kv/src/imps/sled.rs
@@ -0,0 +1,24 @@
+use crate::{KeyValueStore, KeyValueStoreGet, KeyValueStoreInsert, Value};
+use std::borrow::{Borrow, Cow};
+use std::hash::Hash;
+
+impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> KeyValueStoreInsert<K, V> for sled::Tree {
+    fn insert(&mut self, k: K, v: V) {
+        sled::Tree::insert(self, k, v.serialize()).expect("sled io error");
+    }
+}
+
+impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> KeyValueStoreGet<K, V> for sled::Tree {
+    fn get<Q: ?Sized>(&self, k: &Q) -> Option<Cow<V>>
+    where
+        K: Borrow<Q>,
+        Q: Ord + Hash + Eq + AsRef<[u8]>,
+    {
+        sled::Tree::get(self, k)
+            .expect("sled io error")
+            .map(|vec| Value::deserialize(vec.as_ref()))
+            .map(Cow::Owned)
+    }
+}
+
+impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> KeyValueStore<K, V> for sled::Tree {}
---
+++ crates/kv/src/lib.rs
@@ -0,0 +1,45 @@
+//! Abstract KV-Store interface.
+
+use std::borrow::{Borrow, Cow};
+use std::hash::Hash;
+
+mod imps;
+
+/// Value trait
+pub trait Value: ToOwned<Owned = Self> {
+    /// Serialize value to bytes
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if the value cannot be serialized.
+    #[cfg(feature = "sled")]
+    fn serialize(&self) -> Vec<u8>;
+    /// Deserialize value from bytes
+    ///
+    /// # Panics
+    ///
+    /// This function may panic if the bytes are not a valid encoding of the value.
+    #[cfg(feature = "sled")]
+    fn deserialize(buf: &[u8]) -> Self;
+}
+
+/// Key-Value store insert trait
+pub trait KeyValueStoreInsert<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> {
+    /// Insert key-value pair
+    fn insert(&mut self, k: K, v: V);
+}
+
+/// Key-Value store trait
+pub trait KeyValueStoreGet<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> {
+    /// Get value by key
+    fn get<Q: ?Sized>(&self, k: &Q) -> Option<Cow<V>>
+    where
+        K: Borrow<Q>,
+        Q: Ord + Hash + Eq + AsRef<[u8]>;
+}
+
+/// Key-Value store trait
+pub trait KeyValueStore<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value>:
+    KeyValueStoreInsert<K, V> + KeyValueStoreGet<K, V>
+{
+}
