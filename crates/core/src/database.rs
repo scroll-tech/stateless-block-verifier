17eabdc57ebfe6ce5c1e9fed1a8516e3ca799f9a
lightsing <light.tsing@gmail.com>
2024-12-10 14:15:21 +0800

    new database

--- crates/core/src/database.rs
+++ crates/core/src/database.rs
@@ -2,128 +2,50 @@ use crate::error::DatabaseError;
 use revm::interpreter::analysis::to_analysed;
 use revm::{
     db::DatabaseRef,
-    primitives::{AccountInfo, Address, Bytecode, B256, U256},
+    primitives::{AccountInfo, Address, Bytecode, Bytes, B256, U256},
 };
-use sbv_primitives::{
-    zk_trie::{
-        db::{
-            kv::{KVDatabase, KVDatabaseItem},
-            NodeDb,
-        },
-        hash::{key_hasher::NoCacheHasher, poseidon::Poseidon, ZkHash},
-        scroll_types::Account,
-        trie::ZkTrie,
-    },
-    Block,
-};
+use sbv_kv::{KeyValueStore, KeyValueStoreGet};
+use sbv_trie::{PartialStateTrie, TrieNode};
+use std::convert::Infallible;
 use std::{cell::RefCell, collections::HashMap, fmt};
 
 type Result<T, E = DatabaseError> = std::result::Result<T, E>;
 
 /// A database that consists of account and storage information.
-pub struct EvmDatabase<'a, CodeDb, ZkDb> {
+pub struct EvmDatabase<CodeDb, NodesProvider> {
     /// Map of code hash to bytecode.
-    pub(crate) code_db: &'a mut CodeDb,
+    code_db: CodeDb,
     /// Cache of analyzed code
     analyzed_code_cache: RefCell<HashMap<B256, Option<Bytecode>>>,
-    /// Storage root cache, avoid re-query account when storage root is needed
-    storage_root_caches: RefCell<HashMap<Address, ZkHash>>,
-    /// Storage trie cache, avoid re-creating trie for the same account.
-    /// Need to invalidate before `update`, otherwise the trie root may be outdated
-    storage_trie_caches: RefCell<HashMap<ZkHash, Option<ZkTrie<Poseidon>>>>,
-    /// Current uncommitted zkTrie root based on the block trace.
-    committed_zktrie_root: B256,
-    /// The underlying zkTrie database.
-    pub(crate) zktrie_db: &'a mut NodeDb<ZkDb>,
-    /// Current view of zkTrie database.
-    zktrie: ZkTrie<Poseidon>,
+    /// Provider of trie nodes
+    nodes_provider: NodesProvider,
+    /// partial merkle patricia trie
+    state: PartialStateTrie,
 }
 
-impl<CodeDb, Db> fmt::Debug for EvmDatabase<'_, CodeDb, Db> {
+impl<CodeDb, Db> fmt::Debug for EvmDatabase<CodeDb, Db> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("EvmDatabase")
-            .field("committed_zktrie_root", &self.committed_zktrie_root)
-            .finish()
+        f.debug_struct("EvmDatabase").finish()
     }
 }
 
-impl<'a, CodeDb: KVDatabase, ZkDb: KVDatabase + 'static> EvmDatabase<'a, CodeDb, ZkDb> {
+impl<CodeDb: KeyValueStoreGet<B256, Bytes>, NodesProvider: KeyValueStoreGet<B256, TrieNode>>
+    EvmDatabase<CodeDb, NodesProvider>
+{
     /// Initialize an EVM database from a zkTrie root.
     pub fn new_from_root(
-        committed_zktrie_root: B256,
-        code_db: &'a mut CodeDb,
-        zktrie_db: &'a mut NodeDb<ZkDb>,
+        code_db: CodeDb,
+        state_root_before: B256,
+        nodes_provider: NodesProvider,
     ) -> Result<Self> {
-        let zktrie = ZkTrie::new_with_root(zktrie_db, NoCacheHasher, committed_zktrie_root)
-            .map_err(DatabaseError::zk_trie)?;
+        let state = PartialStateTrie::open(&nodes_provider, state_root_before);
 
         Ok(EvmDatabase {
             code_db,
             analyzed_code_cache: Default::default(),
-            storage_root_caches: Default::default(),
-            storage_trie_caches: Default::default(),
-            committed_zktrie_root,
-            zktrie_db,
-            zktrie,
+            nodes_provider,
+            state,
         })
-    }
-
-    /// Get the previous storage root of an account.
-    #[inline]
-    pub(crate) fn prev_storage_root(&self, address: &Address) -> B256 {
-        self.storage_root_caches
-            .borrow()
-            .get(address)
-            .copied()
-            .unwrap_or_default()
-    }
-
-    #[inline]
-    pub(crate) fn update_storage_root_cache(&self, address: Address, storage_root: ZkTrie) {
-        let new_root = *storage_root.root().unwrap_ref();
-        let old = self
-            .storage_root_caches
-            .borrow_mut()
-            .insert(address, new_root);
-
-        let mut storage_trie_caches = self.storage_trie_caches.borrow_mut();
-        if let Some(old) = old {
-            storage_trie_caches.remove(&old);
-        }
-
-        storage_trie_caches.insert(new_root, Some(storage_root));
-    }
-
-    /// Get the committed zkTrie root.
-    #[inline]
-    pub(crate) fn committed_zktrie_root(&self) -> B256 {
-        self.committed_zktrie_root
-    }
-
-    /// Set the committed zkTrie root.
-    #[inline]
-    pub(crate) fn updated_committed_zktrie_root(&mut self, new_root: B256) {
-        self.committed_zktrie_root = new_root;
-    }
-
-    /// Update the database with a new block trace.
-    pub fn insert_codes<T: Block>(&mut self, l2_trace: T) -> Result<()> {
-        measure_duration_millis!(
-            update_db_duration_milliseconds,
-            self.insert_codes_inner(l2_trace)
-        )
-    }
-
-    fn insert_codes_inner<T: Block>(&mut self, l2_trace: T) -> Result<()> {
-        cycle_tracker_start!("insert CodeDB");
-        for code in l2_trace.codes() {
-            let hash = revm::primitives::keccak256(code);
-            self.code_db
-                .or_put(hash.as_slice(), code)
-                .map_err(DatabaseError::code_db)?;
-        }
-        cycle_tracker_end!("insert CodeDB");
-        Ok(())
     }
 
     fn load_code(&self, hash: B256) -> Result<Option<Bytecode>> {
@@ -134,45 +56,37 @@ impl<'a, CodeDb: KVDatabase, ZkDb: KVDatabase + 'static> EvmDatabase<'a, CodeDb,
             let code = self
                 .code_db
                 .get(&hash)
-                .map_err(DatabaseError::code_db)?
-                .map(|v| to_analysed(Bytecode::new_legacy(v.into_bytes().into())));
+                .map(|v| to_analysed(Bytecode::new_legacy(v.into_owned())));
             code_cache.insert(hash, code.clone());
             Ok(code)
         }
     }
 }
 
-impl<CodeDb: KVDatabase, ZkDb: KVDatabase + 'static> DatabaseRef for EvmDatabase<'_, CodeDb, ZkDb> {
-    type Error = DatabaseError;
+impl<CodeDb, NodesProvider> DatabaseRef for EvmDatabase<CodeDb, NodesProvider> {
+    type Error = Infallible;
 
     /// Get basic account information.
     fn basic_ref(&self, address: Address) -> Result<Option<AccountInfo>> {
-        let Some(account) = measure_duration_micros!(
-            zktrie_get_duration_microseconds,
-            self.zktrie.get::<_, Account, _>(self.zktrie_db, address)
-        )
-        .map_err(DatabaseError::zk_trie)?
-        else {
+        let Some(account) = self.state.get_account(address) else {
             return Ok(None);
         };
+        dev_trace!("load trie account of {:?}", address);
 
-        self.storage_root_caches
-            .borrow_mut()
-            .insert(address, account.storage_root);
+        let mut info = AccountInfo {
+            balance: account.balance,
+            nonce: account.nonce,
+            code_hash: account.code_hash,
+            code: self.load_code(account.code_hash)?,
+            ..Default::default()
+        };
 
-        let mut info = AccountInfo::from(account);
-        info.code = self.load_code(account.code_hash)?;
-
+        #[cfg(debug_assertions)]
         if let Some(ref code) = info.code {
-            debug_assert_eq!(
+            assert_eq!(
                 info.code_hash,
                 code.hash_slow(),
                 "code hash mismatch for account {address:?}",
-            );
-            assert_eq!(
-                info.code_size,
-                code.original_bytes().len(),
-                "code size mismatch for account {address:?}",
             );
         }
 
@@ -197,57 +111,10 @@ impl<CodeDb: KVDatabase, ZkDb: KVDatabase + 'static> DatabaseRef for EvmDatabase
     /// Get storage value of address at index.
     fn storage_ref(&self, address: Address, index: U256) -> Result<U256> {
         dev_trace!("get storage of {:?} at index {:?}", address, index);
-        let storage_root = *self
-            .storage_root_caches
-            .borrow_mut()
-            .entry(address)
-            .or_insert_with_key(|address| {
-                self.zktrie
-                    .get::<_, Account, _>(self.zktrie_db, address)
-                    .expect("unexpected zktrie error")
-                    .map(|acc| acc.storage_root)
-                    .unwrap_or_default()
-            });
-
-        let mut storage_trie_caches = self.storage_trie_caches.borrow_mut();
-
-        let trie = storage_trie_caches
-            .entry(storage_root)
-            .or_insert_with_key(|storage_root| {
-                dev_debug!("storage root of {:?} is {:?}", address, storage_root);
-
-                ZkTrie::new_with_root(self.zktrie_db, NoCacheHasher, *storage_root)
-                    .inspect_err(|_e| {
-                        dev_warn!(
-                            "storage trie associated with account({address}) not found: {_e}\n{}",
-                            std::backtrace::Backtrace::force_capture()
-                        );
-                    })
-                    .ok()
-            });
-        if trie.is_none() {
-            return Err(DatabaseError::NotIncluded);
-        }
-        let trie = trie.as_mut().unwrap();
-
-        #[cfg(debug_assertions)]
-        {
-            let current_root = trie.root().unwrap_ref();
-            let expected_root = self
-                .zktrie
-                .get::<_, Account, _>(self.zktrie_db, address)
-                .expect("unexpected zktrie error")
-                .map(|acc| acc.storage_root)
-                .unwrap_or_default();
-            assert_eq!(*current_root, expected_root);
-        }
-
-        Ok(measure_duration_micros!(
-            zktrie_get_duration_microseconds,
-            trie.get::<_, U256, _>(self.zktrie_db, index.to_be_bytes::<32>())
-        )
-        .map_err(DatabaseError::zk_trie)?
-        .unwrap_or_default())
+        Ok(self
+            .state
+            .get_storage(&self.nodes_provider, address, index)
+            .unwrap_or(U256::ZERO))
     }
 
     /// Get block hash by block number.
