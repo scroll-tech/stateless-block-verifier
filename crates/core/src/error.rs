3d3d00c76000891c934b71ed3433d8c22ae69485
lightsing <light.tsing@gmail.com>
2024-12-12 15:20:10 +0800

    remove DatabaseError

--- crates/core/src/error.rs
+++ crates/core/src/error.rs
@@ -1,31 +1,14 @@
 use revm::primitives::{alloy_primitives::SignatureError, EVMError, B256};
-use std::error::Error;
-
-/// Error variants encountered during manipulation of a zkTrie.
-#[derive(Debug, thiserror::Error)]
-pub enum DatabaseError {
-    /// Error encountered from code db.
-    #[error("error encountered from code db: {0}")]
-    CodeDb(Box<dyn Error + Send + Sync>),
-    /// Error encountered from zkTrie.
-    #[error("error encountered from zkTrie: {0}")]
-    ZkTrie(Box<dyn Error + Send + Sync>),
-    /// Error when try to load un-included account/storage.
-    #[error("account/storage not included in zkTrie")]
-    NotIncluded,
-}
+use std::convert::Infallible;
 
 /// Error variants encountered during verification of transactions in a L2 block.
 #[derive(Debug, thiserror::Error)]
 pub enum VerificationError {
-    /// Error while operating on the database.
-    #[error("database error: {0}")]
-    Database(#[from] DatabaseError),
     /// Error while recovering signer from an ECDSA signature.
-    #[error("invalid signature for tx_hash={tx_hash}: {source}")]
+    #[error("invalid signature for #{idx} tx: {source}")]
     InvalidSignature {
-        /// The tx hash.
-        tx_hash: B256,
+        /// The idx of the transaction in the block.
+        idx: usize,
         /// The source error that occurred while recovering signer.
         source: SignatureError,
     },
@@ -41,7 +24,7 @@ pub enum VerificationError {
         /// The tx hash.
         tx_hash: B256,
         /// The source error originating in [`revm`].
-        source: EVMError<DatabaseError>,
+        source: EVMError<Infallible>,
     },
     /// Root mismatch error
     #[error("root_after in trace doesn't match with root_after in revm: root_trace={root_trace}, root_revm={root_revm}")]
@@ -51,22 +34,4 @@ pub enum VerificationError {
         /// Root after in revm
         root_revm: B256,
     },
-}
-
-impl DatabaseError {
-    pub(crate) fn code_db<E: Error + Send + Sync + 'static>(err: E) -> Self {
-        dev_error!(
-            "code_db error {err} occurred in:\n{}",
-            std::backtrace::Backtrace::force_capture()
-        );
-        DatabaseError::CodeDb(Box::new(err))
-    }
-
-    pub(crate) fn zk_trie<E: Error + Send + Sync + 'static>(err: E) -> Self {
-        dev_error!(
-            "zk_trie error {err} occurred in:\n{}",
-            std::backtrace::Backtrace::force_capture()
-        );
-        DatabaseError::ZkTrie(Box::new(err))
-    }
 }
