4cd78f0df67ab056d7656c08077a5c46bb3d2697
lightsing <light.tsing@gmail.com>
2024-12-12 15:20:56 +0800

    update builder

--- crates/core/src/executor/builder.rs
+++ crates/core/src/executor/builder.rs
@@ -1,110 +1,119 @@
-use crate::{error::DatabaseError, EvmDatabase, EvmExecutor, HardforkConfig};
+use crate::{EvmDatabase, EvmExecutor};
 use revm::db::CacheDB;
-use sbv_primitives::{
-    alloy_primitives::ChainId, zk_trie::db::kv::KVDatabase, zk_trie::db::NodeDb, B256,
-};
+use sbv_chainspec::ChainSpec;
+use sbv_kv::KeyValueStore;
+use sbv_primitives::alloy_primitives::Bytes;
+use sbv_primitives::{keccak256, BlockWitness, B256};
+use sbv_trie::TrieNode;
 use std::fmt::{self, Debug};
 
 /// Builder for EVM executor.
-pub struct EvmExecutorBuilder<'a, H, C, CodeDb, ZkDb> {
-    hardfork_config: H,
-    chain_id: C,
-    code_db: &'a mut CodeDb,
-    zktrie_db: &'a mut NodeDb<ZkDb>,
+pub struct EvmExecutorBuilder<Spec, CodeDb, NodesProvider, Witness> {
+    chain_spec: Spec,
+    code_db: CodeDb,
+    nodes_provider: NodesProvider,
+    witness: Witness,
 }
 
-impl<H: Debug, C: Debug, CodeDb, ZkDb> Debug for EvmExecutorBuilder<'_, H, C, CodeDb, ZkDb> {
+impl Debug for EvmExecutorBuilder<(), (), (), ()> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        f.debug_struct("EvmExecutorBuilder")
-            .field("hardfork_config", &self.hardfork_config)
-            .field("chain_id", &self.chain_id)
-            .field("code_db", &"...")
-            .field("zktrie_db", &"...")
-            .finish()
+        f.debug_struct("EvmExecutorBuilder").finish()
     }
 }
 
-impl<'a, CodeDb, ZkDb> EvmExecutorBuilder<'a, (), (), CodeDb, ZkDb> {
-    /// Create a new builder.
-    pub fn new(code_db: &'a mut CodeDb, zktrie_db: &'a mut NodeDb<ZkDb>) -> Self {
+impl Default for EvmExecutorBuilder<(), (), (), ()> {
+    fn default() -> Self {
         Self {
-            hardfork_config: (),
-            chain_id: (),
-            code_db,
-            zktrie_db,
+            chain_spec: (),
+            code_db: (),
+            nodes_provider: (),
+            witness: (),
         }
     }
 }
 
-impl<'a, H, C, CodeDb, ZkDb> EvmExecutorBuilder<'a, H, C, CodeDb, ZkDb> {
+impl<Spec, CodeDb, NodesProvider, Witness>
+    EvmExecutorBuilder<Spec, CodeDb, NodesProvider, Witness>
+{
+    /// Create a new EVM executor builder.
+    fn new() -> EvmExecutorBuilder<(), (), (), ()> {
+        EvmExecutorBuilder::default()
+    }
+
     /// Set hardfork config.
-    pub fn hardfork_config<H1>(
+    pub fn chain_spec<S>(
         self,
-        hardfork_config: H1,
-    ) -> EvmExecutorBuilder<'a, H1, C, CodeDb, ZkDb> {
+        chain_spec: S,
+    ) -> EvmExecutorBuilder<S, CodeDb, NodesProvider, Witness> {
         EvmExecutorBuilder {
-            hardfork_config,
-            chain_id: self.chain_id,
+            chain_spec,
             code_db: self.code_db,
-            zktrie_db: self.zktrie_db,
+            nodes_provider: self.nodes_provider,
+            witness: self.witness,
         }
     }
 
-    /// Set chain id.
-    pub fn chain_id<C1>(self, chain_id: C1) -> EvmExecutorBuilder<'a, H, C1, CodeDb, ZkDb> {
+    /// Set code database.
+    pub fn code_db<C>(self, code_db: C) -> EvmExecutorBuilder<Spec, C, NodesProvider, Witness> {
         EvmExecutorBuilder {
-            hardfork_config: self.hardfork_config,
-            chain_id,
-            code_db: self.code_db,
-            zktrie_db: self.zktrie_db,
-        }
-    }
-
-    /// Set code db.
-    pub fn code_db<CodeDb1>(
-        self,
-        code_db: &'a mut CodeDb1,
-    ) -> EvmExecutorBuilder<'a, H, C, CodeDb1, ZkDb> {
-        EvmExecutorBuilder {
-            hardfork_config: self.hardfork_config,
-            chain_id: self.chain_id,
+            chain_spec: self.chain_spec,
             code_db,
-            zktrie_db: self.zktrie_db,
+            nodes_provider: self.nodes_provider,
+            witness: self.witness,
         }
     }
 
-    /// Set zktrie db.
-    pub fn zktrie_db<ZkDb1>(
+    /// Set nodes provider.
+    pub fn nodes_provider<N>(
         self,
-        zktrie_db: &'a mut NodeDb<ZkDb1>,
-    ) -> EvmExecutorBuilder<H, C, CodeDb, ZkDb1> {
+        nodes_provider: N,
+    ) -> EvmExecutorBuilder<Spec, CodeDb, N, Witness> {
         EvmExecutorBuilder {
-            hardfork_config: self.hardfork_config,
-            chain_id: self.chain_id,
+            chain_spec: self.chain_spec,
             code_db: self.code_db,
-            zktrie_db,
+            nodes_provider,
+            witness: self.witness,
+        }
+    }
+
+    /// Set witness.
+    pub fn witness<W>(self, witness: W) -> EvmExecutorBuilder<Spec, CodeDb, NodesProvider, W> {
+        EvmExecutorBuilder {
+            chain_spec: self.chain_spec,
+            code_db: self.code_db,
+            nodes_provider: self.nodes_provider,
+            witness,
         }
     }
 }
 
-impl<'a, CodeDb: KVDatabase, ZkDb: KVDatabase + 'static>
-    EvmExecutorBuilder<'a, HardforkConfig, ChainId, CodeDb, ZkDb>
+impl<
+        CodeDb: KeyValueStore<B256, Bytes>,
+        NodesProvider: KeyValueStore<B256, TrieNode>,
+        Witness: BlockWitness,
+    > EvmExecutorBuilder<ChainSpec, CodeDb, NodesProvider, Witness>
 {
     /// Initialize an EVM executor from a block trace as the initial state.
-    pub fn build(self, root: B256) -> Result<EvmExecutor<'a, CodeDb, ZkDb>, DatabaseError> {
+    pub fn build(mut self, root: B256) -> EvmExecutor<CodeDb, NodesProvider, Witness> {
+        for code in self.witness.codes_iter() {
+            let code = code.as_ref();
+            let code_hash = keccak256(code);
+            self.code_db.insert(code_hash, Bytes::copy_from_slice(code))
+        }
+
         let db = cycle_track!(
             CacheDB::new(EvmDatabase::new_from_root(
-                root,
                 self.code_db,
-                self.zktrie_db
-            )?),
+                root,
+                self.nodes_provider
+            )),
             "build ReadOnlyDB"
         );
 
-        Ok(EvmExecutor {
-            hardfork_config: self.hardfork_config,
-            chain_id: self.chain_id,
+        EvmExecutor {
+            chain_spec: self.chain_spec,
             db,
-        })
+            witness: self.witness,
+        }
     }
 }
