bb6ee82df7a4cc9f461a5314935be1fe9490ba7a
lightsing <light.tsing@gmail.com>
2024-12-12 12:33:41 +0800

    use auto_impl

--- crates/kv/src/imps/mod.rs
+++ crates/kv/src/imps/mod.rs
@@ -1,46 +1,5 @@
-use crate::{KeyValueStore, KeyValueStoreGet, KeyValueStoreInsert, Value};
-use std::borrow::{Borrow, Cow};
-use std::hash::Hash;
-
 #[cfg(feature = "alloy-trie")]
 mod alloy_trie;
 #[cfg(feature = "sled")]
 mod sled;
 mod std_collections;
-
-impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value, S: KeyValueStore<K, V>> KeyValueStoreGet<K, V>
-    for &S
-{
-    fn get<Q: ?Sized>(&self, k: &Q) -> Option<Cow<V>>
-    where
-        K: Borrow<Q>,
-        Q: Ord + Hash + Eq + AsRef<[u8]>,
-    {
-        S::get(*self, k)
-    }
-}
-
-impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value, S: KeyValueStore<K, V>> KeyValueStoreGet<K, V>
-    for &mut S
-{
-    fn get<Q: ?Sized>(&self, k: &Q) -> Option<Cow<V>>
-    where
-        K: Borrow<Q>,
-        Q: Ord + Hash + Eq + AsRef<[u8]>,
-    {
-        S::get(*self, k)
-    }
-}
-
-impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value, S: KeyValueStore<K, V>> KeyValueStoreInsert<K, V>
-    for &mut S
-{
-    fn insert(&mut self, k: K, v: V) {
-        S::insert(*self, k, v)
-    }
-}
-
-impl<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value, S: KeyValueStore<K, V>> KeyValueStore<K, V>
-    for &mut S
-{
-}
--- crates/kv/src/lib.rs
+++ crates/kv/src/lib.rs
@@ -1,11 +1,13 @@
 //! Abstract KV-Store interface.
 
+use auto_impl::auto_impl;
 use std::borrow::{Borrow, Cow};
 use std::hash::Hash;
 
 mod imps;
 
 /// Value trait
+#[auto_impl(&, &mut, Box, Rc, Arc)]
 pub trait Value: ToOwned<Owned = Self> {
     /// Serialize value to bytes
     ///
@@ -24,12 +26,14 @@ pub trait Value: ToOwned<Owned = Self> {
 }
 
 /// Key-Value store insert trait
+#[auto_impl(&mut, Box)]
 pub trait KeyValueStoreInsert<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> {
     /// Insert key-value pair
     fn insert(&mut self, k: K, v: V);
 }
 
 /// Key-Value store trait
+#[auto_impl(&, &mut, Box, Rc, Arc)]
 pub trait KeyValueStoreGet<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> {
     /// Get value by key
     fn get<Q: ?Sized>(&self, k: &Q) -> Option<Cow<V>>
@@ -39,6 +43,7 @@ pub trait KeyValueStoreGet<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value> {
 }
 
 /// Key-Value store trait
+#[auto_impl(&, &mut, Box, Rc, Arc)]
 pub trait KeyValueStore<K: Ord + Hash + Eq + AsRef<[u8]>, V: Value>:
     KeyValueStoreInsert<K, V> + KeyValueStoreGet<K, V>
 {
