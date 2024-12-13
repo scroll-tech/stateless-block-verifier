b767bd13f58cc293e883f7525de4c24cf4d0cadf
lightsing <light.tsing@gmail.com>
2024-12-13 17:07:05 +0800

    fix

--- scripts/download.sh
+++ scripts/download.sh
@@ -16,10 +16,4 @@ HEX_BLOCK=$(printf '0x%x' $BLOCK)
 mkdir -p ${OUT_DIR}/${HEX_BLOCK}
 
 cast rpc -r $URL eth_getBlockByNumber "$HEX_BLOCK" true | jq > ${OUT_DIR}/${HEX_BLOCK}/block.json
-#PAYLOAD='{"jsonrpc":"2.0","method":"debug_executionWitness", "params": ["'$HEX_BLOCK'"], "id": 1}'
-#curl -H "Content-Type: application/json" -X POST --data "$PAYLOAD" $URL | jq .result > ${OUT_DIR}/${HEX_BLOCK}/witness.json
-#
-#PAYLOAD='{"jsonrpc":"2.0","method":"eth_getBlockByNumber", "params": ["'$HEX_BLOCK'", true], "id": 1}'
-#curl -H "Content-Type: application/json" -X POST --data "$PAYLOAD" $URL | jq .result > ${OUT_DIR}/${HEX_BLOCK}/block.json
-##cast rpc -r $URL debug_executionWitness "$HEX_BLOCK" > ${OUT_DIR}/${HEX_BLOCK}/witness.json
-##
+cast rpc -r $URL debug_executionWitness "$HEX_BLOCK" > ${OUT_DIR}/${HEX_BLOCK}/witness.json
