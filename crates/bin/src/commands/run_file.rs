0e47c482d51d4f308129eda42a1f1bd60faf6ccf
lightsing <light.tsing@gmail.com>
2024-12-13 17:06:28 +0800

    update bin

--- crates/bin/src/commands/run_file.rs
+++ crates/bin/src/commands/run_file.rs
@@ -1,50 +1,32 @@
 use crate::utils;
-use anyhow::{anyhow, bail};
+use anyhow::anyhow;
 use clap::Args;
-use sbv::{
-    core::{BlockExecutionResult, ChunkInfo, EvmExecutorBuilder, HardforkConfig},
-    primitives::{
-        types::{BlockTrace, LegacyStorageTrace},
-        zk_trie::db::kv::HashMapDb,
-        Block, B256,
-    },
-};
-use serde::Deserialize;
+use sbv::primitives::types::BlockWitness;
 use std::panic::catch_unwind;
 use std::path::PathBuf;
-use tiny_keccak::{Hasher, Keccak};
 use tokio::task::JoinSet;
 
 #[derive(Args)]
 pub struct RunFileCommand {
-    /// Path to the trace file
-    #[arg(default_value = "trace.json")]
+    /// Path to the witness file
+    #[arg(default_value = "witness.json")]
     path: Vec<PathBuf>,
-    /// Chunk mode
-    #[arg(short, long)]
-    chunk_mode: bool,
+    // FIXME
+    // /// Chunk mode
+    // #[arg(short, long)]
+    // chunk_mode: bool,
 }
 
 impl RunFileCommand {
-    pub async fn run(
-        self,
-        fork_config: impl Fn(u64) -> HardforkConfig + Send + Sync + Copy + 'static,
-    ) -> anyhow::Result<()> {
-        if self.chunk_mode {
-            self.run_chunk(fork_config).await
-        } else {
-            self.run_traces(fork_config).await
-        }
+    pub async fn run(self) -> anyhow::Result<()> {
+        self.run_witnesses().await
     }
 
-    async fn run_traces(
-        self,
-        fork_config: impl Fn(u64) -> HardforkConfig + Send + Sync + Copy + 'static,
-    ) -> anyhow::Result<()> {
+    async fn run_witnesses(self) -> anyhow::Result<()> {
         let mut tasks = JoinSet::new();
 
         for path in self.path.into_iter() {
-            tasks.spawn(run_trace(path, fork_config));
+            tasks.spawn(run_witness(path));
         }
 
         while let Some(task) = tasks.join_next().await {
@@ -56,114 +38,88 @@ impl RunFileCommand {
         Ok(())
     }
 
-    async fn run_chunk(
-        self,
-        fork_config: impl Fn(u64) -> HardforkConfig + Send + Sync + Copy + 'static,
-    ) -> anyhow::Result<()> {
-        let traces = futures::future::join_all(self.path.iter().map(read_block_trace))
-            .await
-            .into_iter()
-            .collect::<Result<Vec<BlockTrace>, _>>()?;
-
-        let has_same_chain_id = traces
-            .iter()
-            .all(|trace| trace.chain_id() == traces[0].chain_id());
-        if !has_same_chain_id {
-            bail!("All traces must have the same chain id in chunk mode");
-        }
-
-        let has_seq_block_number = traces
-            .windows(2)
-            .all(|w| w[0].number() + 1 == w[1].number());
-        if !has_seq_block_number {
-            bail!("All traces must have sequential block numbers in chunk mode");
-        }
-
-        let fork_config = fork_config(traces[0].chain_id());
-        let (chunk_info, mut zktrie_db) = ChunkInfo::from_block_traces(&traces);
-        let mut code_db = HashMapDb::default();
-
-        let mut tx_bytes_hasher = Keccak::v256();
-
-        let mut executor = EvmExecutorBuilder::new(&mut code_db, &mut zktrie_db)
-            .hardfork_config(fork_config)
-            .chain_id(traces[0].chain_id())
-            .build(traces[0].root_before())?;
-        for trace in traces.iter() {
-            executor.insert_codes(trace)?;
-        }
-
-        for trace in traces.iter() {
-            let BlockExecutionResult { tx_rlps, .. } = executor.handle_block(trace)?;
-            for tx_rlp in tx_rlps {
-                tx_bytes_hasher.update(&tx_rlp);
-            }
-        }
-
-        let post_state_root = executor.commit_changes()?;
-        if post_state_root != chunk_info.post_state_root() {
-            bail!("post state root mismatch");
-        }
-        drop(executor);
-
-        let mut tx_bytes_hash = B256::ZERO;
-        tx_bytes_hasher.finalize(&mut tx_bytes_hash.0);
-        let _public_input_hash = chunk_info.public_input_hash(&tx_bytes_hash);
-
-        dev_info!("[chunk mode] public input hash: {:?}", _public_input_hash);
-
-        Ok(())
-    }
+    // FIXME
+    // async fn run_chunk(
+    //     self,
+    //     fork_config: impl Fn(u64) -> HardforkConfig + Send + Sync + Copy + 'static,
+    // ) -> anyhow::Result<()> {
+    //     let traces = futures::future::join_all(self.path.iter().map(read_block_trace))
+    //         .await
+    //         .into_iter()
+    //         .collect::<Result<Vec<BlockTrace>, _>>()?;
+    //
+    //     let has_same_chain_id = traces
+    //         .iter()
+    //         .all(|trace| trace.chain_id() == traces[0].chain_id());
+    //     if !has_same_chain_id {
+    //         bail!("All traces must have the same chain id in chunk mode");
+    //     }
+    //
+    //     let has_seq_block_number = traces
+    //         .windows(2)
+    //         .all(|w| w[0].number() + 1 == w[1].number());
+    //     if !has_seq_block_number {
+    //         bail!("All traces must have sequential block numbers in chunk mode");
+    //     }
+    //
+    //     let fork_config = fork_config(traces[0].chain_id());
+    //     let (chunk_info, mut zktrie_db) = ChunkInfo::from_block_traces(&traces);
+    //     let mut code_db = HashMapDb::default();
+    //
+    //     let mut tx_bytes_hasher = Keccak::v256();
+    //
+    //     let mut executor = EvmExecutorBuilder::new(&mut code_db, &mut zktrie_db)
+    //         .hardfork_config(fork_config)
+    //         .chain_id(traces[0].chain_id())
+    //         .build(traces[0].root_before())?;
+    //     for trace in traces.iter() {
+    //         executor.insert_codes(trace)?;
+    //     }
+    //
+    //     for trace in traces.iter() {
+    //         let BlockExecutionResult { tx_rlps, .. } = executor.handle_block(trace)?;
+    //         for tx_rlp in tx_rlps {
+    //             tx_bytes_hasher.update(&tx_rlp);
+    //         }
+    //     }
+    //
+    //     let post_state_root = executor.commit_changes()?;
+    //     if post_state_root != chunk_info.post_state_root() {
+    //         bail!("post state root mismatch");
+    //     }
+    //     drop(executor);
+    //
+    //     let mut tx_bytes_hash = B256::ZERO;
+    //     tx_bytes_hasher.finalize(&mut tx_bytes_hash.0);
+    //     let _public_input_hash = chunk_info.public_input_hash(&tx_bytes_hash);
+    //
+    //     dev_info!("[chunk mode] public input hash: {:?}", _public_input_hash);
+    //
+    //     Ok(())
+    // }
 }
 
-async fn read_block_trace(path: &PathBuf) -> anyhow::Result<BlockTrace> {
-    let trace = tokio::fs::read_to_string(&path).await?;
-    tokio::task::spawn_blocking(move || deserialize_block_trace(&trace)).await?
+async fn read_witness(path: &PathBuf) -> anyhow::Result<BlockWitness> {
+    let witness = tokio::fs::read(&path).await?;
+    let jd = &mut serde_json::Deserializer::from_slice(&witness);
+    let witness = serde_path_to_error::deserialize::<_, BlockWitness>(jd)?;
+    Ok(witness)
 }
 
-fn deserialize_block_trace(trace: &str) -> anyhow::Result<BlockTrace> {
-    let block_trace = deserialize_may_wrapped::<BlockTrace>(trace)?;
-    if block_trace.storage_trace.flatten_proofs.is_empty() {
-        dev_warn!("Storage trace is empty, try to deserialize as legacy storage trace");
-        let legacy_trace = deserialize_may_wrapped::<BlockTrace<LegacyStorageTrace>>(trace)?;
-        return Ok(legacy_trace.into());
-    }
-    Ok(block_trace)
-}
-fn deserialize_may_wrapped<'de, T: Deserialize<'de>>(trace: &'de str) -> anyhow::Result<T> {
-    // Try to deserialize `BlockTrace` from JSON. In case of failure, try to
-    // deserialize `BlockTrace` from a JSON-RPC response that has the actual block
-    // trace nested in the value of the key "result".
-    Ok(serde_json::from_str::<T>(trace).or_else(|_| {
-        #[derive(serde::Deserialize, Default, Debug, Clone)]
-        pub struct BlockTraceJsonRpcResult<T> {
-            pub result: T,
-        }
-        Ok::<_, serde_json::Error>(
-            serde_json::from_str::<BlockTraceJsonRpcResult<T>>(trace)?.result,
-        )
-    })?)
-}
-
-async fn run_trace(
-    path: PathBuf,
-    fork_config: impl Fn(u64) -> HardforkConfig,
-) -> anyhow::Result<()> {
-    let trace = read_block_trace(&path).await?;
-    let fork_config = fork_config(trace.chain_id());
-    if let Err(e) =
-        tokio::task::spawn_blocking(move || catch_unwind(|| utils::verify(&trace, &fork_config)))
-            .await?
-            .map_err(|e| {
-                e.downcast_ref::<&str>()
-                    .map(|s| anyhow!("task panics with: {s}"))
-                    .or_else(|| {
-                        e.downcast_ref::<String>()
-                            .map(|s| anyhow!("task panics with: {s}"))
-                    })
-                    .unwrap_or_else(|| anyhow!("task panics"))
-            })
-            .and_then(|r| r.map_err(anyhow::Error::from))
+async fn run_witness(path: PathBuf) -> anyhow::Result<()> {
+    let witness = read_witness(&path).await?;
+    if let Err(e) = tokio::task::spawn_blocking(move || catch_unwind(|| utils::verify(&witness)))
+        .await?
+        .map_err(|e| {
+            e.downcast_ref::<&str>()
+                .map(|s| anyhow!("task panics with: {s}"))
+                .or_else(|| {
+                    e.downcast_ref::<String>()
+                        .map(|s| anyhow!("task panics with: {s}"))
+                })
+                .unwrap_or_else(|| anyhow!("task panics"))
+        })
+        .and_then(|r| r.map_err(anyhow::Error::from))
     {
         dev_error!(
             "Error occurs when verifying block ({}): {:?}",
--- crates/bin/src/utils.rs
+++ crates/bin/src/utils.rs
@@ -1,34 +1,20 @@
-use sbv::primitives::zk_trie::db::NodeDb;
 use sbv::{
-    core::{EvmExecutorBuilder, HardforkConfig, VerificationError},
-    primitives::{zk_trie::db::kv::HashMapDb, Block},
+    chainspec::{Chain, ChainSpecProvider, WellKnownChainSpecProvider},
+    core::{EvmExecutorBuilder, VerificationError},
+    primitives::{BlockHeader, BlockWitness, Bytes, B256},
+    trie::TrieNode,
 };
+use std::collections::HashMap;
 
-pub fn verify<T: Block + Clone>(
-    l2_trace: T,
-    fork_config: &HardforkConfig,
-) -> Result<(), VerificationError> {
+pub fn verify<T: BlockWitness>(witness: T) -> Result<(), VerificationError> {
     measure_duration_millis!(
         total_block_verification_duration_milliseconds,
-        verify_inner(l2_trace, fork_config)
+        verify_inner(witness)
     )
 }
 
-fn verify_inner<T: Block + Clone>(
-    l2_trace: T,
-    fork_config: &HardforkConfig,
-) -> Result<(), VerificationError> {
-    dev_trace!("{l2_trace:#?}");
-    let root_before = l2_trace.root_before();
-    let root_after = l2_trace.root_after();
-
-    // or with v2 trace
-    // let v2_trace = BlockTraceV2::from(l2_trace.clone());
-
-    // or with rkyv zero copy
-    // let serialized = rkyv::to_bytes::<BlockTraceV2, 4096>(&v2_trace).unwrap();
-    // let archived = unsafe { rkyv::archived_root::<BlockTraceV2>(&serialized[..]) };
-    // let archived = rkyv::check_archived_root::<BlockTraceV2>(&serialized[..]).unwrap();
+fn verify_inner<T: BlockWitness>(witness: T) -> Result<(), VerificationError> {
+    dev_trace!("{witness:#?}");
 
     #[cfg(feature = "profiling")]
     let guard = pprof::ProfilerGuardBuilder::default()
@@ -37,40 +23,33 @@ fn verify_inner<T: Block + Clone>(
         .build()
         .unwrap();
 
-    let mut zktrie_db = cycle_track!(
-        {
-            let mut zktrie_db = NodeDb::new(HashMapDb::default());
-            measure_duration_millis!(
-                build_zktrie_db_duration_milliseconds,
-                l2_trace.build_zktrie_db(&mut zktrie_db).unwrap()
-            );
-            zktrie_db
-        },
-        "build ZktrieState"
-    );
-    let mut code_db = HashMapDb::default();
+    let chain_spec = WellKnownChainSpecProvider::new(Chain::from_id(witness.chain_id()))
+        .unwrap()
+        .chain_spec();
+    let mut code_db = HashMap::<B256, Bytes>::new();
+    let mut nodes_provider = HashMap::<B256, TrieNode>::new();
 
-    let mut executor = EvmExecutorBuilder::new(&mut code_db, &mut zktrie_db)
-        .hardfork_config(*fork_config)
-        .chain_id(l2_trace.chain_id())
-        .build(root_before)?;
-
-    executor.insert_codes(&l2_trace)?;
+    let mut executor = EvmExecutorBuilder::default()
+        .chain_spec(chain_spec)
+        .code_db(&mut code_db)
+        .nodes_provider(&mut nodes_provider)
+        .witness(&witness)
+        .build();
 
     // TODO: change to Result::inspect_err when sp1 toolchain >= 1.76
     #[allow(clippy::map_identity)]
     #[allow(clippy::manual_inspect)]
-    executor.handle_block(&l2_trace).map_err(|e| {
+    executor.handle_block().map_err(|e| {
         dev_error!(
             "Error occurs when executing block #{}({:?}): {e:?}",
-            l2_trace.number(),
-            l2_trace.block_hash()
+            witness.header().number(),
+            witness.header().hash(),
         );
 
         update_metrics_counter!(verification_error);
         e
     })?;
-    let revm_root_after = executor.commit_changes()?;
+    let revm_post_state_root = executor.commit_changes();
 
     #[cfg(feature = "profiling")]
     if let Ok(report) = guard.report().build() {
@@ -78,30 +57,32 @@ fn verify_inner<T: Block + Clone>(
             .join(env!("CARGO_PKG_NAME"))
             .join("profiling");
         std::fs::create_dir_all(&dir).unwrap();
-        let path = dir.join(format!("block-{}.svg", l2_trace.number()));
+        let path = dir.join(format!("block-{}.svg", witness.header().number()));
         let file = std::fs::File::create(&path).unwrap();
         report.flamegraph(file).unwrap();
         dev_info!("Profiling report saved to: {:?}", path);
     }
 
-    if root_after != revm_root_after {
+    if witness.header().state_root() != revm_post_state_root {
         dev_error!(
-            "Block #{}({:?}) root mismatch: root after in trace = {root_after:x}, root after in revm = {revm_root_after:x}",
-            l2_trace.number(),
-            l2_trace.block_hash(),
+            "Block #{}({:?}) root mismatch: root after in trace = {:x}, root after in revm = {:x}",
+            witness.header().number(),
+            witness.header().hash(),
+            witness.header().state_root(),
+            revm_post_state_root
         );
 
         update_metrics_counter!(verification_error);
 
         return Err(VerificationError::RootMismatch {
-            root_trace: root_after,
-            root_revm: revm_root_after,
+            root_trace: witness.header().state_root(),
+            root_revm: revm_post_state_root,
         });
     }
     dev_info!(
         "Block #{}({}) verified successfully",
-        l2_trace.number(),
-        l2_trace.block_hash(),
+        witness.header().number(),
+        witness.header().hash(),
     );
     Ok(())
 }
