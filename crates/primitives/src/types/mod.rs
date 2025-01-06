3684cf65894ddab1fa950448e6ed11adefffd4e3
lightsing <light.tsing@gmail.com>
2025-01-06 14:57:55 +0800

    add DiskRoot rpc response type

--- crates/primitives/src/types/mod.rs
+++ crates/primitives/src/types/mod.rs
@@ -1,5 +1,7 @@
 mod access_list;
 mod block_header;
+#[cfg(feature = "scroll")]
+mod scroll;
 mod signature;
 mod transaction;
 mod withdrawal;
@@ -8,10 +10,11 @@ mod witness;
 pub use access_list::{AccessList, AccessListItem, ArchivedAccessList, ArchivedAccessListItem};
 pub use alloy_consensus::{Header as AlloyHeader, TypedTransaction as AlloyTypedTransaction};
 pub use alloy_eips::eip4895::{Withdrawal as AlloyWithdrawal, Withdrawals as AlloyWithdrawals};
-pub use alloy_rpc_types_debug::ExecutionWitness;
 pub use alloy_rpc_types_eth::Block as RpcBlock;
 pub use block_header::{ArchivedBlockHeader, BlockHeader};
+#[cfg(feature = "scroll")]
+pub use scroll::*;
 pub use signature::{ArchivedSignature, Signature};
 pub use transaction::{ArchivedTransaction, Transaction};
 pub use withdrawal::{ArchivedWithdrawal, Withdrawal};
-pub use witness::{ArchivedBlockWitness, BlockWitness};
+pub use witness::{ArchivedBlockWitness, BlockWitness, ExecutionWitness};
