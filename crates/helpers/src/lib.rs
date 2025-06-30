f545895f8b61476df470c26b3685d89f50e27cd9
Akase Haruka <lightsing@users.noreply.github.com>
2025-06-30 14:27:11 +0800

    chore: refactor cleanup (#110)
    * cleanup
    
    * fmt
    
    * bump openvm

--- crates/helpers/src/lib.rs
+++ crates/helpers/src/lib.rs
@@ -5,10 +5,6 @@ pub use tracing;
 #[macro_use]
 mod macros;
 
-mod utils;
-#[cfg(any(feature = "debug-account", feature = "debug-storage"))]
-pub use utils::debug::DebugRecorder;
-
 /// Metrics module
 #[cfg(feature = "metrics")]
 #[doc(hidden)]
