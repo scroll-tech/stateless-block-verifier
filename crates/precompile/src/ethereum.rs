dee0e89bb8b509ba8426e5bb12cab2e34e46d8e8
lightsing <light.tsing@gmail.com>
2025-06-30 14:37:22 +0800

    add mainnet precompile

--- crates/precompile/src/ethereum.rs
+++ crates/precompile/src/ethereum.rs
@@ -1,5 +1,8 @@
 use super::PrecompileProvider;
-use sbv_primitives::types::{evm::precompiles::PrecompilesMap, revm::precompile::PrecompileSpecId};
+use sbv_primitives::types::{
+    evm::precompiles::PrecompilesMap,
+    revm::precompile::{PrecompileSpecId, Precompiles},
+};
 
 #[cfg(not(feature = "ethereum-openvm"))]
 impl PrecompileProvider {
@@ -13,7 +16,23 @@ impl PrecompileProvider {
 #[cfg(feature = "ethereum-openvm")]
 impl PrecompileProvider {
     /// Returns the precompiles map for the given spec.
-    pub fn with_spec(_spec: PrecompileSpecId) -> PrecompilesMap {
-        todo!()
+    pub fn with_spec(spec: PrecompileSpecId) -> PrecompilesMap {
+        use crate::imps::{bn128, kzg_point_evaluation, secp256k1, sha256};
+
+        let mut precompiles = Precompiles::new(spec).to_owned();
+
+        precompiles.extend([secp256k1::ECRECOVER, sha256::HOMESTEAD]);
+
+        if spec >= PrecompileSpecId::ISTANBUL {
+            precompiles.extend([bn128::add::ISTANBUL, bn128::mul::ISTANBUL]);
+        } else if spec >= PrecompileSpecId::BYZANTIUM {
+            precompiles.extend([bn128::add::BYZANTIUM, bn128::mul::BYZANTIUM]);
+        }
+
+        if spec >= PrecompileSpecId::CANCUN {
+            precompiles.extend([kzg_point_evaluation::POINT_EVALUATION]);
+        }
+
+        PrecompilesMap::new(std::borrow::Cow::Owned(precompiles))
     }
 }
--- crates/precompile/src/imps/mod.rs
+++ crates/precompile/src/imps/mod.rs
@@ -2,6 +2,14 @@
 #[cfg_attr(docsrs, doc(cfg(feature = "bn128")))]
 pub mod bn128;
 
+#[cfg(feature = "kzg")]
+#[cfg_attr(docsrs, doc(cfg(feature = "kzg")))]
+pub mod kzg_point_evaluation;
+
+#[cfg(feature = "secp256k1")]
+#[cfg_attr(docsrs, doc(cfg(feature = "secp256k1")))]
+pub mod secp256k1;
+
 #[cfg(feature = "sha256")]
 #[cfg_attr(docsrs, doc(cfg(feature = "sha256")))]
 pub mod sha256;
