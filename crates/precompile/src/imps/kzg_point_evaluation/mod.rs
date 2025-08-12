ad273a321cedf26115e262fe6aa1be6527fdbea9
Akase Haruka <lightsing@users.noreply.github.com>
2025-08-12 12:32:50 +0800

    chore: upgrade (#120)
    * upgrade alloy, revm and reth
    
    * fmt
    
    * fix openvm kzg

--- crates/precompile/src/imps/kzg_point_evaluation/mod.rs
+++ crates/precompile/src/imps/kzg_point_evaluation/mod.rs
@@ -8,16 +8,16 @@ compile_error!(
 use sbv_primitives::types::revm::precompile::{PrecompileWithAddress, kzg_point_evaluation};
 
 pub use kzg_point_evaluation::{
-    ADDRESS, GAS_COST, RETURN_VALUE, VERSIONED_HASH_VERSION_KZG, as_array, kzg_to_versioned_hash,
+    ADDRESS, GAS_COST, RETURN_VALUE, VERSIONED_HASH_VERSION_KZG, kzg_to_versioned_hash,
 };
 
 #[cfg(not(feature = "openvm-kzg"))]
-pub use kzg_point_evaluation::{as_bytes32, as_bytes32, run, verify_kzg_proof};
+pub use kzg_point_evaluation::{run, verify_kzg_proof};
 
 #[cfg(feature = "openvm-kzg")]
 mod openvm;
 #[cfg(feature = "openvm-kzg")]
-pub use openvm::{as_bytes32, as_bytes48, run, verify_kzg_proof};
+pub use openvm::{run, verify_kzg_proof};
 
 /// KZG point evaluation precompile, containing address and function to run.
 pub const POINT_EVALUATION: PrecompileWithAddress = PrecompileWithAddress(ADDRESS, run);
