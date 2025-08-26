1b9aa95e992c0766d9da554c7191aa03fe4c472b
lightsing <light.tsing@gmail.com>
2025-08-26 16:51:45 +0800

    Revert "chunk mode ok"
    This reverts commit 4ec7e7dbd40e68ee5a64f1613eb492409a6f2ccc.

--- crates/core/src/verifier/scroll.rs
+++ crates/core/src/verifier/scroll.rs
@@ -1,8 +1,8 @@
-use crate::{DatabaseError, EvmDatabase, EvmExecutor, VerificationError};
+use crate::{EvmDatabase, EvmExecutor, VerificationError};
 use itertools::Itertools;
 use sbv_kv::{nohash::NoHashMap, null::NullProvider};
 use sbv_primitives::{
-    B256, Bytes, U256,
+    B256, Bytes,
     chainspec::ChainSpec,
     ext::{BlockWitnessChunkExt, BlockWitnessExt},
     types::{
@@ -20,6 +20,7 @@ use std::{collections::BTreeMap, sync::Arc};
     derive(rkyv::Archive, rkyv::Deserialize, rkyv::Serialize),
     rkyv(derive(Debug, Hash, PartialEq, Eq))
 )]
+#[deprecated(note = "StateCommitMode is no longer used and will be removed in future versions.")]
 pub enum StateCommitMode {
     /// Commit state by chunk.
     Chunk,
@@ -48,7 +49,6 @@ pub struct VerifyResult {
 pub fn run(
     witnesses: Vec<BlockWitness>,
     chain_spec: Arc<ChainSpec>,
-    state_commit_mode: StateCommitMode,
     compression_ratios: Option<
         impl IntoIterator<Item = impl IntoIterator<Item = impl Into<sbv_primitives::U256>>> + Clone,
     >,
@@ -68,6 +68,8 @@ pub fn run(
     let nodes_provider = manually_drop_on_zkvm!(nodes_provider);
 
     let pre_state_root = witnesses[0].prev_state_root;
+    let post_state_root = witnesses.last().unwrap().header.state_root;
+
     let blocks = witnesses
         .into_iter()
         .map(|w| {
@@ -76,39 +78,68 @@ pub fn run(
         })
         .collect::<Result<Vec<RecoveredBlock<Block>>, _>>()?;
 
-    let mut args = ExecuteInnerArgs {
-        code_db: &code_db,
-        nodes_provider: &nodes_provider,
+    if !blocks
+        .iter()
+        .tuple_windows()
+        .all(|(a, b)| a.hash() == b.header().parent_hash)
+    {
+        return Err(VerificationError::ParentHashMismatch);
+    }
+
+    let mut db = manually_drop_on_zkvm!(EvmDatabase::new_from_root(
+        code_db,
         pre_state_root,
-        blocks: &blocks,
-        chain_spec: chain_spec.clone(),
-        defer_commit: true,
-        compression_ratios,
-    };
+        &nodes_provider,
+        NullProvider,
+    )?);
 
-    let result = match state_commit_mode {
-        StateCommitMode::Chunk | StateCommitMode::Block => {
-            args.defer_commit = matches!(state_commit_mode, StateCommitMode::Chunk);
-            execute(args)?
+    let mut gas_used = 0;
+
+    let mut execute_block = |block, compression_ratio| -> Result<(), VerificationError> {
+        let output = manually_drop_on_zkvm!(
+            EvmExecutor::new(chain_spec.clone(), &db, block, compression_ratio).execute()?
+        );
+
+        gas_used += output.gas_used;
+
+        db.update(
+            &nodes_provider,
+            BTreeMap::from_iter(output.state.state.clone()).iter(),
+        )?;
+
+        let post_state_root = db.commit_changes();
+        if block.state_root != post_state_root {
+            dev_error!(
+                "Block #{} root mismatch: root after in trace = {:x}, root after in reth = {:x}",
+                block.number,
+                block.state_root,
+                post_state_root
+            );
+            return Err(VerificationError::root_mismatch(
+                block.state_root,
+                post_state_root,
+                #[cfg(not(target_os = "zkvm"))]
+                output.state,
+            ));
         }
-        StateCommitMode::Auto => match execute(args.clone()) {
-            Ok(result) => result,
-            Err(VerificationError::Database(DatabaseError::PartialStateTrie(_e))) => {
-                dev_warn!(
-                    "Failed to execute with defer commit enabled: {_e}; retrying with defer commit disabled"
-                );
-                #[cfg(target_os = "zkvm")]
-                {
-                    println!("failed to update db: {_e}; retrying with defer commit disabled");
-                }
-                args.defer_commit = false;
-                execute(args)?
-            }
-            Err(e) => return Err(e),
-        },
+
+        Ok(())
     };
 
-    let (post_state_root, withdraw_root, gas_used) = result;
+    if let Some(compression_ratios) = compression_ratios {
+        for (block, compression_ratios) in blocks.iter().zip_eq(compression_ratios) {
+            execute_block(
+                block,
+                Some(compression_ratios.into_iter().map(|u| u.into())),
+            )?;
+        }
+    } else {
+        for block in blocks.iter() {
+            execute_block(block, None)?;
+        }
+    }
+
+    let withdraw_root = db.withdraw_root()?;
 
     Ok(VerifyResult {
         blocks,
@@ -144,109 +175,6 @@ fn make_providers(witnesses: &[BlockWitness]) -> (CodeDb, NodesProvider) {
     (code_db, nodes_provider)
 }
 
-#[derive(Clone)]
-pub(super) struct ExecuteInnerArgs<'a, I> {
-    pub(super) code_db: &'a CodeDb,
-    pub(super) nodes_provider: &'a NodesProvider,
-    pub(super) pre_state_root: B256,
-    pub(super) blocks: &'a [RecoveredBlock<Block>],
-    pub(super) chain_spec: Arc<ChainSpec>,
-    pub(super) defer_commit: bool,
-    pub(super) compression_ratios: Option<I>,
-}
-
-#[inline]
-fn execute<II, I, R>(
-    ExecuteInnerArgs {
-        code_db,
-        nodes_provider,
-        pre_state_root,
-        blocks,
-        chain_spec,
-        defer_commit,
-        compression_ratios,
-    }: ExecuteInnerArgs<II>,
-) -> Result<(B256, B256, u64), VerificationError>
-where
-    II: IntoIterator<Item = I>,
-    I: IntoIterator<Item = R>,
-    R: Into<U256>,
-{
-    let mut gas_used = 0;
-
-    let mut db = manually_drop_on_zkvm!(EvmDatabase::new_from_root(
-        code_db,
-        pre_state_root,
-        nodes_provider,
-        NullProvider,
-    )?);
-
-    let mut execute_block = |block, compression_ratio| -> Result<(), VerificationError> {
-        let output = manually_drop_on_zkvm!(
-            EvmExecutor::new(chain_spec.clone(), &db, block, compression_ratio).execute()?
-        );
-
-        gas_used += output.gas_used;
-
-        db.update(
-            nodes_provider,
-            BTreeMap::from_iter(output.state.state.clone()).iter(),
-        )?;
-
-        if !defer_commit {
-            let post_state_root = db.commit_changes();
-            if block.state_root != post_state_root {
-                dev_error!(
-                    "Block #{} root mismatch: root after in trace = {:x}, root after in reth = {:x}",
-                    block.number,
-                    block.state_root,
-                    post_state_root
-                );
-                return Err(VerificationError::root_mismatch(
-                    block.state_root,
-                    post_state_root,
-                    #[cfg(not(target_os = "zkvm"))]
-                    Some(Box::new(output.state)),
-                ));
-            }
-            dev_info!("Block #{} verified successfully", block.number);
-        } else {
-            dev_info!("Block #{} executed successfully", block.number);
-        }
-
-        Ok(())
-    };
-
-    if let Some(compression_ratios) = compression_ratios {
-        for (block, compression_ratios) in blocks.iter().zip_eq(compression_ratios) {
-            execute_block(
-                block,
-                Some(compression_ratios.into_iter().map(|u| u.into())),
-            )?;
-        }
-    } else {
-        for block in blocks {
-            execute_block(block, None)?;
-        }
-    }
-
-    let post_state_root = db.commit_changes();
-    let expected_state_root = blocks.last().unwrap().state_root;
-    if expected_state_root != post_state_root {
-        dev_error!(
-            "Final state root mismatch: expected {expected_state_root:x}, found {post_state_root:x}",
-        );
-        return Err(VerificationError::root_mismatch(
-            expected_state_root,
-            post_state_root,
-            #[cfg(not(target_os = "zkvm"))]
-            None,
-        ));
-    }
-    let withdraw_root = db.withdraw_root()?;
-    Ok((post_state_root, withdraw_root, gas_used))
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -266,13 +194,7 @@ mod tests {
         let witness: BlockWitness = serde_json::from_str(witness_json).unwrap();
         let chain_spec =
             build_chain_spec_force_hardfork(Chain::from_id(witness.chain_id), Hardfork::EuclidV2);
-        run(
-            vec![witness],
-            chain_spec,
-            StateCommitMode::Block,
-            None::<Vec<Vec<U256>>>,
-        )
-        .unwrap();
+        run(vec![witness], chain_spec, None::<Vec<Vec<U256>>>).unwrap();
     }
 
     #[rstest::rstest]
@@ -284,12 +206,6 @@ mod tests {
         let witness: BlockWitness = serde_json::from_str(witness_json).unwrap();
         let chain_spec =
             build_chain_spec_force_hardfork(Chain::from_id(witness.chain_id), Hardfork::Feynman);
-        run(
-            vec![witness],
-            chain_spec,
-            StateCommitMode::Block,
-            None::<Vec<Vec<U256>>>,
-        )
-        .unwrap();
+        run(vec![witness], chain_spec, None::<Vec<Vec<U256>>>).unwrap();
     }
 }
