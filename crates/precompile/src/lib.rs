bce7178351e99b553c4a1d92b78bbce6a4f0d641
lightsing <light.tsing@gmail.com>
2025-09-17 14:53:01 +0800

    rename to bn254

--- crates/precompile/src/lib.rs
+++ crates/precompile/src/lib.rs
@@ -1,15 +1,15 @@
 //! sbv precompiles provider
 #![cfg_attr(docsrs, feature(doc_cfg))]
 #[cfg(any(
-    feature = "openvm-bn128",
+    feature = "openvm-bn254",
     feature = "openvm-kzg",
     feature = "openvm-secp256k1",
 ))]
 use sbv_primitives::types::revm::precompile::PrecompileError;
 use sbv_primitives::types::revm::precompile::{Crypto as CryptoInterface, install_crypto};
 
-#[cfg(feature = "openvm-bn128")]
-mod bn128;
+#[cfg(feature = "openvm-bn254")]
+mod bn254;
 #[cfg(feature = "openvm-kzg")]
 mod kzg_point_evaluation;
 #[cfg(feature = "openvm-secp256k1")]
@@ -37,28 +37,28 @@ impl CryptoInterface for Crypto {
         openvm_sha2::sha256(input)
     }
 
-    #[cfg(feature = "openvm-bn128")]
+    #[cfg(feature = "openvm-bn254")]
     #[inline]
     fn bn254_g1_add(&self, p1: &[u8], p2: &[u8]) -> Result<[u8; 64], PrecompileError> {
-        let p1 = bn128::read_g1_point(p1)?;
-        let p2 = bn128::read_g1_point(p2)?;
-        let result = bn128::g1_point_add(p1, p2);
-        Ok(bn128::encode_g1_point(result))
+        let p1 = bn254::read_g1_point(p1)?;
+        let p2 = bn254::read_g1_point(p2)?;
+        let result = bn254::g1_point_add(p1, p2);
+        Ok(bn254::encode_g1_point(result))
     }
 
-    #[cfg(feature = "openvm-bn128")]
+    #[cfg(feature = "openvm-bn254")]
     #[inline]
     fn bn254_g1_mul(&self, point: &[u8], scalar: &[u8]) -> Result<[u8; 64], PrecompileError> {
-        let p = bn128::read_g1_point(point)?;
-        let fr = bn128::read_scalar(scalar);
-        let result = bn128::g1_point_mul(p, fr);
-        Ok(bn128::encode_g1_point(result))
+        let p = bn254::read_g1_point(point)?;
+        let fr = bn254::read_scalar(scalar);
+        let result = bn254::g1_point_mul(p, fr);
+        Ok(bn254::encode_g1_point(result))
     }
 
-    #[cfg(feature = "openvm-bn128")]
+    #[cfg(feature = "openvm-bn254")]
     #[inline]
     fn bn254_pairing_check(&self, pairs: &[(&[u8], &[u8])]) -> Result<bool, PrecompileError> {
-        bn128::pairing_check(pairs)
+        bn254::pairing_check(pairs)
     }
 
     #[cfg(feature = "openvm-secp256k1")]
